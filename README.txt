48b4c9f8408a0463324aed1fb219f413160a8ef8
Venzene <vansh.singh8272@gmail.com>
2023-11-09 15:47:16 +0530

    This is me trying to understand the selenium webdriver module!

--- README.txt
+++ README.txt
@@ -9,3 +9,11 @@ Let see if ti works or not
 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
 
 This is the peronal token ig!
+
+These are the videos you wanna watch to learn this course
+
+PRELIMBS
+
+https://www.youtube.com/watch?v=JeznW_7DlB0
+
+
--- automate.py
+++ automate.py
@@ -1,2 +1,33 @@
-import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys #This is the class of different keys ex.RETURN
+import time
 
+
+#This is the driver object instancated form the chrome() class
+driver = webdriver.Chrome()
+
+
+#Get the link you wanted to search
+driver.get("https://amazon.com")
+
+#Wait till the page loads //clear this code and find a new function.
+#This is errorsome
+
+driver.implicitly_wait(5)
+
+#This is a search object instanceated form the find_element class
+
+search = driver.find_element("id","twotabsearchtextbox")
+
+#sending the key arguments in the methods provided by the element class!!
+search.send_keys("pokemon cards")
+search.send_keys(Keys.RETURN)
+
+source_code = driver.page_source
+
+file = open("/home/vansha/Desktop/Project/automate/Automate/pokemon.html","w")
+file.write(source_code)
+
+time.sleep(10)
+
+driver.quit
